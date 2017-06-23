818c79c0348429cd01ea68b2d318f063edf0165e
Gonzalo Arias <gonzarias@gmail.com>
2017-06-23 20:02:44 -0300

    Add metodo perimetro
    Agregado de metodo perimetro

--- src/com/FiguraPoligono.java
+++ src/com/FiguraPoligono.java
@@ -1,13 +1,13 @@
 package com;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
-
-import static com.sun.jmx.mbeanserver.Util.cast;
 
 
 public class FiguraPoligono{
     private Polygon poligono;
+    private ArrayList<Point2D.Double> points;
 
     public FiguraPoligono(ArrayList<Planeta> planetas){
         int i = planetas.size();
@@ -19,6 +19,8 @@ public class FiguraPoligono{
             int x = (int)planeta.posicion.getX();
             int y = (int)planeta.posicion.getY();
 
+            // Agregado para calcular perimetro
+            points.add(new Point2D.Double(x,y));
 
             xPoints[j] = x;
             yPoints[j] = y;
@@ -31,6 +33,37 @@ public class FiguraPoligono{
         Posicion posicionSol = sol.getPosicion();
 
         return this.poligono.contains(posicionSol.getX(),posicionSol.getY());
+    }
+
+    public double perimetro() {
+
+
+        if (points.size() < 2){
+            return 0.0;
+        }
+
+        int i = 0;
+        double d = 0;
+        double total = 0;
+
+        while (i < points.size() - 1 )
+        {
+            Point2D.Double point1 = points.get(i);
+            double x = point1.x;
+            double y = point1.y;
+            Point2D.Double point2 = points.get(i+1);
+            double x1 = point2.x;
+            double y1 = point2.y;
+
+            d = point1.distance(point2);
+            System.out.println(d);
+            //d = Math.sqrt(Math.pow(x1 - x,2) + Math.pow(y1 - y, 2));
+            total = total + d;
+            i++;
+
+        }
+        return total;
+
     }
 
 }
